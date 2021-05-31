37465893c3a61c6bff160e9d3273521b8e3e431e
Mayur Paunipagar <mayur.paunipagar@taskmo.com>
2021-05-31 16:23:59 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ function App() {
   return (
     <div className="App">
       <div className="sidebarHome">
-        
-          <img src={window.location.origin + "/images/taskmoLogo.svg"} alt={"logo"} className="logoImg" />
-          <div className="QCText">Quality Check</div>
-          <div className="dbText">Dashboard</div>
-          <div className="homeText">Home</div>
+        <img
+          src={window.location.origin + "/images/taskmoLogo.svg"}
+          alt={"logo"}
+          className="logoImg"
+        />
+        <div className="QCText">Quality Check</div>
+        <div className="dbText">Dashboard</div>
+        <div className="homeText">Home</div>
       </div>
-      
-      <div className="main-content">
 
+      <div className="main-content">
+        <div>TASKMO</div>
       </div>
     </div>
   );
