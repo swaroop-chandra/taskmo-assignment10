3b32b02a91ac3d7e40818cb53bdff319f85c386b
Mayur Paunipagar <mayur.paunipagar@taskmo.com>
2021-06-04 11:15:25 +0530

    router added

--- src/component/main-content.js
+++ src/component/main-content.js
@@ -1,10 +1,21 @@
 import MyForm from "./my-form";
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 export default function MainContent({blur}) {
   return (
     <>
       <div className={`main-content ${blur?"myBlur":null}`}>
-        <MyForm/>
+        <Router>
+          <Switch>
+            <Route path="/qc">
+            <MyForm/>
+            </Route>
+          </Switch>
+        </Router>
       </div>
     </>
   );
--- src/component/my-form.js
+++ src/component/my-form.js
@@ -1,6 +1,12 @@
 import { optionsPie } from "../dataset/chartDataset";
 import Chart from "react-apexcharts";
 import QcscoreComp2 from "./qcscore-comp2";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 export default function MyForm() {
   return (
@@ -155,8 +161,12 @@ export default function MyForm() {
               width="200px"
             />
           </div>
+          <Route path="/qc/select-status">
           <QcscoreComp2 />
-          <div className="redoText">REDO</div>
+          </Route>
+         <Route path="/qc/redo">
+         <div className="redoText">REDO</div>
+         </Route>
         </div>
       </div>
     </>
