eefe0d62bb912dbbfe281133c2b3a0adac83e91c
swaroop-chandra <swaroop.chandra666@gmail.com>
2021-06-05 20:10:12 +0530

    dashboard added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import "./App.css";
 import MainContent from "./component/main-content";
 import QcCheckDone from "./component/qc-check-done";
 import SideBar from "./component/sidebar";
+import Dashboard from "./component/dashboard/dashboard";
 
 function App() {
-  const [blur,setBlur]=useState(false);
+  const [blur, setBlur] = useState(false);
   return (
     <div className="App">
-      <QcCheckDone blur={blur} setBlur={setBlur}/>
-      <SideBar blur={blur} setBlur={setBlur}/>
-      <MainContent blur={blur}/>
+      {/* <QcCheckDone blur={blur} setBlur={setBlur} /> */}
+      <SideBar blur={blur} setBlur={setBlur} />
+      <MainContent blur={blur} />
     </div>
   );
 }
--- src/component/main-content.js
+++ src/component/main-content.js
@@ -1,19 +1,16 @@
 import MyForm from "./my-form";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-export default function MainContent({blur}) {
+import Dashboard from "./dashboard/dashboard";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+export default function MainContent({ blur }) {
   return (
     <>
-      <div className={`main-content ${blur?"myBlur":null}`}>
+      <div className={`main-content ${blur ? "myBlur" : null}`}>
         <Router>
           <Switch>
             <Route path="/qc">
-            <MyForm/>
+              <MyForm />
             </Route>
+            <Route path="/dash" component={Dashboard} />
           </Switch>
         </Router>
       </div>
