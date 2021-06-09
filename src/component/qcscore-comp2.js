111eaf849ae8ed49f03cf2d7a07569db491a3209
Mayur Paunipagar <mayur.paunipagar@taskmo.com>
2021-06-09 17:27:26 +0530

    redo reject approve function added

--- src/component/qcscore-comp2.js
+++ src/component/qcscore-comp2.js
@@ -1,22 +1,65 @@
-export default function QcscoreComp2({setBlur,qcScore}){
-    return <>
-    <div className="qcscoreComp2Container">
-
+import { useState } from "react";
+import "./qcscore-comp2.css";
+export default function QcscoreComp2({ setBlur, qcScore }) {
+  const [fakeOnBoarding, setFakeOnBoarding] = useState(false);
+  return (
+    <>
+      <div className="qcscoreComp2Container">
         <div className="qcscoreComment">
-            <input type="text" className="commentInput" placeholder={qcScore===100?"":"Enter Comment"} disabled={qcScore===100}/>
+          <input
+            type="text"
+            className="commentInput"
+            placeholder={qcScore === 100 ? "" : "Enter Comment"}
+            disabled={qcScore === 100}
+          />
         </div>
         <div className="invalidInputText qcInvalid">
-              <img src={window.location.origin+"/images/warningIcon.svg"} alt="warning icon" className="warningIcon"/>
-
-                Email ID not verified. Please verify again!
-              </div>
-        <div className="btnsContainer">
-            <button className="qcScoreBtn">REDO</button>
-            <button className="qcScoreBtn">REJECT</button>
-            <button className="qcScoreBtn" disabled={qcScore!==100} onClick={()=>{
-                setBlur(true);
-            }}>APPROVE</button>
+          <img
+            src={window.location.origin + "/images/warningIcon.svg"}
+            alt="warning icon"
+            className="warningIcon"
+          />
+          Email ID not verified. Please verify again!
         </div>
-    </div>
+        <div className="fakeOnBoarding">
+          <input
+            type="checkbox"
+            onChange={(e) => {
+                setFakeOnBoarding(e.target.checked);
+            }}
+          />{" "}
+          Fake Onboarding
+        </div>
+        <div className="btnsContainer">
+          <button
+            className={`qcScoreBtn ${
+              (qcScore < 100) && (!fakeOnBoarding) ? "activateQcScoreBtn" : ""
+            }`}
+            disabled={(qcScore === 100)||(fakeOnBoarding)}
+          >
+            REDO
+          </button>
+          <button
+            className={`qcScoreBtn ${
+              qcScore < 100 ? "activateQcScoreBtn" : ""
+            }`}
+            disabled={(qcScore === 100)||(!fakeOnBoarding)}
+          >
+            REJECT
+          </button>
+          <button
+            className={`qcScoreBtn ${
+              (qcScore === 100) && !fakeOnBoarding ? "activateQcScoreBtn" : ""
+            }`}
+            disabled={(qcScore !== 100) || fakeOnBoarding}
+            onClick={() => {
+              setBlur(true);
+            }}
+          >
+            APPROVE
+          </button>
+        </div>
+      </div>
     </>
+  );
 }
