f543b13da3ea605aaf35ab9a25a5f7e2cce8ee3f
Mayur Paunipagar <mayur.paunipagar@taskmo.com>
2021-06-11 23:04:43 +0530

    added extra comment condition to redo btn

--- src/component/qcscore-comp2.js
+++ src/component/qcscore-comp2.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./qcscore-comp2.css";
 export default function QcscoreComp2({ setBlur, qcScore }) {
   const [fakeOnBoarding, setFakeOnBoarding] = useState(false);
+  const [comment,setComment]=useState("");
   return (
     <>
       <div className="qcscoreComp2Container">
@@ -11,6 +12,7 @@ export default function QcscoreComp2({ setBlur, qcScore }) {
             className="commentInput"
             placeholder={qcScore === 100 ? "" : "Enter Comment"}
             disabled={qcScore === 100}
+            onChange={(e)=>{setComment(e.target.value)}}
           />
         </div><div className="fakeOnBoarding">
           <input
@@ -33,9 +35,9 @@ export default function QcscoreComp2({ setBlur, qcScore }) {
         <div className="btnsContainer">
           <button
             className={`qcScoreBtn ${
-              (qcScore < 100) && (!fakeOnBoarding) ? "activateQcScoreBtn" : ""
+              (qcScore < 100) && (!fakeOnBoarding) && !(comment.trim().length===0) ? "activateQcScoreBtn" : ""
             }`}
-            disabled={(qcScore === 100)||(fakeOnBoarding)}
+            disabled={(qcScore === 100) || comment.trim().length===0 ||(fakeOnBoarding)}
           >
             REDO
           </button>
