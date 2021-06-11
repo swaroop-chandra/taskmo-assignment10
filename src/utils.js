828cf7d731b6d9eddd90ce930f5998aef985ba0b
Mayur Paunipagar <mayur.paunipagar@taskmo.com>
2021-06-11 23:05:47 +0530

    added image url

--- src/utils.js
+++ src/utils.js
@@ -3,6 +3,7 @@ const NINJACART_LEAD_FIELDS_URL = "http://localhost:8000/v1/ninjacart/lead_detai
 const NINJACART_QC_REMARKS_URL = "http://localhost:8000/v1/ninjacart/qc_remarks";
 const JIOMART_LEAD_FIELDS_URL = "http://localhost:8000/v1/ninjacart/lead_details";
 const JIOMART_QC_REMARKS_URL = "http://localhost:8000/v1/ninjacart/qc_remarks";
+const IMAGES_BASE_URL = "https:isp.taskmo.in/fieldon_images";
 
 export {
   NINJACART_LEAD_FIELDS_URL,
